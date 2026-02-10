c559fe21b1a58ae90e67dcee7a18d94f271ac8b8
matheus <msantosneves261@gmail.com>
2026-02-10 10:28:29 -0300

    enriquece o modelo de usuarios com mais informações

--- app/app.py
+++ app/app.py
@@ -23,6 +23,9 @@ class Usuario(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
     senha = db.Column(db.String(120), nullable=False)
+    nome = db.Column(db.String(100), nullable=False)
+    descricao = db.Column(db.Text, nullable=False)
+    telefone = db.Column(db.String(20))
 
 
 class Servico(db.Model):
